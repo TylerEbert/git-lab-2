0937aac73d930a3c8daef878a059207ff1a9205f
Tyler Ebert <te516217@ohio.edu>
2019-01-23 19:17:30 -0500

    Created Hello Git program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tyler Ebert
+ *        Date: 01/23/19
+ * Description: Git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
